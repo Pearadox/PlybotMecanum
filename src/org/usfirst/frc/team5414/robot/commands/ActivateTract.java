f31f89c112821dbc17f55fa78a6b5a88478c472a
FRC5414 <pearlandFRC@gmail.com>
2017-03-13 14:56:08 -0500

    Update ActivateTract.java

--- src/org/usfirst/frc/team5414/robot/commands/ActivateTract.java
+++ src/org/usfirst/frc/team5414/robot/commands/ActivateTract.java
@@ -20,7 +20,11 @@ public class ActivateTract extends Command {
 
  
     protected void execute() {
-
+        //I'm not sure this will set the drive back to arcade since isFinished goes to true. 
+        /** Might need one command that does all the solenoids just for autonomous
+         * and one command that does all the solenoids and stays in the execute with out finishing
+         * for teleop command
+         */
     	Robot.drivetrain.arcadeDrive(Robot.oi.getJoystick1());	//sets the drive back to arcade
     }
 
